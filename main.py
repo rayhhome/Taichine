6b6eb555a417bf19ebed9dda8a15295e05db3323
Sirui Huang <siruih@andrew.cmu.edu>
2023-11-01 13:16:00 -0400

    add existing

---
+++ main.py
@@ -0,0 +1,54 @@
+from kivy.app import App
+from kivy.properties import StringProperty
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.image import Image
+from kivy.core.window import Window
+
+from os import listdir
+from os.path import join, isfile
+
+# pose item
+# class PoseItem(ButtonBehavior, Image):
+#   id = StringProperty('')
+#   image = StringProperty('')
+#   label = StringProperty('')
+#   pass
+
+# menu screen
+class MenuScreen(Screen):
+  pass
+
+# selection screen
+class SelectionScreen(Screen):
+  pass
+
+# setting screen
+class SettingScreen(Screen):
+  pass
+
+# training screen
+class TrainingScreen(Screen):
+  pass
+
+class TaichineApp(App):
+  def build(self):
+    Window.size = (720, 540)
+    Window.minimum_width, Window.minimum_height = Window.size    
+    smanager = ScreenManager()
+    smanager.add_widget(MenuScreen())
+    smanager.add_widget(SelectionScreen())
+    smanager.add_widget(SettingScreen())
+    smanager.add_widget(TrainingScreen())
+    # allseqs = listdir('./poses')
+    # for seq in allseqs:
+    #   if isfile(join('./poses', seq)):
+    #     pose = PoseItem()
+    #     pose.id = seq
+    #     pose.image = './poses/' + seq + '/0.jpg'
+    #     pose.label = seq
+    #     smanager.add_widget(pose)
+    return smanager
+    
+if __name__ == '__main__':
+  TaichineApp().run()
